a1e31fbc6db549eba8036de0ce8aaf657f14ed19
Rachna-sys <rachna_it@yahoo.com>
2020-12-18 21:25:51 -0500

    Rachna Java Hackathon

---
+++ src/test/java/com/tekarch/JavaHackathon/Entry_Hackathon.java
@@ -0,0 +1,11 @@
+package com.tekarch.JavaHackathon;
+
+public class Entry_Hackathon {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("My name is Rachna Sakalle");
+	}
+
+}
